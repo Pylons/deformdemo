700875f46be1964239a06ef94621f44c732caa7c
Calvin Hendryx-Parker <calvin@sixfeetup.com>
2013-08-17 10:38:10 +0200

    added webtest for html validation test

--- deformdemo/validation.py
+++ deformdemo/validation.py
@@ -1,7 +1,23 @@
-from pyramid import testing
+import unittest
+from pyramid.paster import bootstrap
 from deformdemo import DeformDemo
 
-request = testing.DummyRequest()
-demos = DeformDemo(request)
 
-import pdb; pdb.set_trace()  # NOQA
+class FunctionalTests(unittest.TestCase):
+    def setUp(self):
+        bs = bootstrap('demo.ini')
+        app = bs['app']
+        self.request = bs['request']
+        from webtest import TestApp
+        self.testapp = TestApp(app)
+        self.demos = DeformDemo(self.request)
+
+    def test_valid_html(self):
+        demos_urls = self.demos.get_demos()
+        for demo in demos_urls:
+            res = self.testapp.get(demo[1], status=200)
+            import pdb; pdb.set_trace()  # NOQA
+        #self.failUnless('Pyramid' in res.body)
+
+if __name__ == '__main__':
+    unittest.main()
