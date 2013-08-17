3452b3c32791a30e8ac2a0bcc6b5a5a70c9b8a14
Charlie Clark <charlie.clark@clark-consulting.eu>
2013-08-17 10:32:46 +0200

    Stub validation service

--- deformdemo/validation.py
+++ deformdemo/validation.py
@@ -1,6 +1,9 @@
 from pyramid import testing
-from deformdemo import DeformDemo
+from deformdemo import DeformDemo, main
 
+config = dict()
+config['deformdemo.renderer'] = 'deformdemo.zpt_renderer'
+main(dict(), **config)
 request = testing.DummyRequest()
 demos = DeformDemo(request)
 
