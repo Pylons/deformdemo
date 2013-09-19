f707e575b62571f34de8bd4c1c6f0f498a6832b9
Domen Kožar <domen@dev.si>
2013-09-19 13:11:16 +0200

    port passwordwidget to webdriver

--- deformdemo/test.py
+++ deformdemo/test.py
@@ -776,40 +776,31 @@ class InternationalizationTests(Base, unittest.TestCase):
 class PasswordWidgetTests(Base, unittest.TestCase):
     url = test_url("/password/")
     def test_render_default(self):
-        browser.get(self.url)
-        self.assertTrue(browser.is_text_present("Password"))
-        self.assertEqual(browser.get_text('css=.required'), 'Password')
-        self.assertEqual(browser.get_text('css=#captured'), 'None')
-        self.assertEqual(browser.get_value('deformField1'), '')
-        self.assertEqual(browser.get_attribute('css=#deformField1@type'),
-                         'password')
-        self.assertFalse(browser.is_element_present('css=.has-error'))
-                         
+        from selenium.common.exceptions import NoSuchElementException
+        self.assertTrue('Password' in browser.page_source)
+        self.assertEqual(browser.find_element_by_id('captured').text, 'None')
+        self.assertRaises(NoSuchElementException, browser.find_element_by_css_selector, '.has-error')
+        self.assertEqual(browser.find_element_by_css_selector('.required').text, 'Password')
+        self.assertEqual(browser.find_element_by_id('deformField1').get_attribute('value'), '')
+
     def test_render_submit_empty(self):
-        browser.get(self.url)
-        browser.click('submit')
-        self.assertTrue(browser.is_text_present("Password"))
-        self.assertEqual(browser.get_text('css=.required'), 'Password')
-        self.assertEqual(browser.get_text('css=#captured'), 'None')
-        self.assertEqual(browser.get_value('deformField1'), '')
-        self.assertEqual(browser.get_attribute('css=#deformField1@type'),
-                         'password')
-        self.assertTrue(browser.is_element_present('css=.has-error'))
-        error_node = 'css=#error-deformField1'
-        self.assertEqual(browser.get_text(error_node), 'Required')
+        browser.find_element_by_id("deformsubmit").click()
+        self.assertTrue('Password' in browser.page_source)
+        self.assertEqual(browser.find_element_by_css_selector('.required').text, 'Password')
+        self.assertEqual(browser.find_element_by_id('captured').text, 'None')
+        self.assertEqual(browser.find_element_by_id('deformField1').get_attribute('value'), '')
+        self.assertEqual(browser.find_element_by_id('error-deformField1').text, 'Required')
 
     def test_render_submit_success(self):
-        browser.get(self.url)
-        browser.type('deformField1', 'abcdef123')
-        browser.click('submit')
-        self.assertTrue(browser.is_text_present("Password"))
+        from selenium.common.exceptions import NoSuchElementException
+        browser.find_element_by_id('deformField1').send_keys('abcdef123')
+        browser.find_element_by_id("deformsubmit").click()
+        self.assertTrue('Password' in browser.page_source)
+        self.assertEqual(browser.find_element_by_id('deformField1').get_attribute('value'), 'abcdef123')
+        self.assertRaises(NoSuchElementException, browser.find_element_by_css_selector, '.has-error')
         self.assertSimilarRepr(
-            browser.get_text('css=#captured'),
+            browser.find_element_by_id('captured').text,
             "{'password': u'abcdef123'}")
-        self.assertEqual(browser.get_value('deformField1'), 'abcdef123')
-        self.assertEqual(browser.get_attribute('css=#deformField1@type'),
-                         'password')
-        self.assertFalse(browser.is_element_present('css=.has-error'))
 
 class RadioChoiceWidgetTests(Base, unittest.TestCase):
     url = test_url("/radiochoice/")
