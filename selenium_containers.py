1f8599cba277860666ceb844bfc55e66414e3855
Sydo Luciani <60237469+sydoluciani@users.noreply.github.com>
2020-09-02 17:28:41 -0500

    docker-py methods to manage selenium docker container.

---
+++ selenium_containers.py
@@ -0,0 +1,21 @@
+import docker
+
+
+def start_firefox():
+    client = docker.from_env()
+    client.containers.run(
+        "selenium/standalone-firefox:4.0.0-alpha-7-prerelease-20200826",
+        ports={'4444/tcp': 4444, '5900/tcp': 5900},
+        volumes={'/dev/shm': {'bind': '/dev/shm', 'mode': 'rw'}, },
+        detach=True,
+        remove=True,
+        auto_remove=True)
+
+
+def stop_selenium_containers():
+    client = docker.from_env()
+    image_name = "selenium/standalone"
+
+    for container in client.containers.list():
+        if image_name in str(container.image.tags):
+            container.stop()
