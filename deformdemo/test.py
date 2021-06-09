273053d6ae7c50b0b7c61d4a24dc3c02a6459b14
Steve Piercy <web@stevepiercy.com>
2021-06-09 01:09:13 -0700

    Remove deliberate failing test, now that Selenium 4.0.0.b has been released with a fix for file uploads.

--- deformdemo/test.py
+++ deformdemo/test.py
@@ -1460,11 +1460,6 @@ class FileUploadTests(Base, unittest.TestCase):
         self.assertEqual(findid("error-deformField1").text, "Required")
         self.assertEqual(findid("captured").text, "None")
 
-    def test_getfile(self):
-        path, filename = _getFile()
-        # Test to show value of `path` via deliberate failure.
-        self.assertEqual("", path)
-
     def test_submit_filled(self):
         # submit one first
         path, filename = _getFile()
